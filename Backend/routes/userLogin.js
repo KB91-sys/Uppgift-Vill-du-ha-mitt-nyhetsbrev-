855e615b9c946e13381806ef9d5aef2023bc0276
Kristofer <kristofer_b_1991@hotmail.com>
2020-05-29 17:03:19 +0200

    Frontend

--- Backend/routes/userLogin.js
+++ Backend/routes/userLogin.js
@@ -1,23 +1,39 @@
 var express = require('express');
 var fs = require('fs');
-
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: true});
 
 var router = express.Router();
 
-
-router.post('/userLogin', function (err, res)
+router.post('/', urlencodedParser, function (req, res)
 {
 
     fs.readFile('regUsers.json', (err, data) => {
 
+
         if(err) throw err;
 
-        var regUsers = JSON.parse(data);
+        var jsonData = JSON.parse(data);
 
+        var userName = req.body.userName;
+        var password = req.body.password;
 
-        regUsers.
+        var userNameArray = jsonData.filter(a => a.userName === userName);
+        
+        var checkPassword = userNameArray.find(a => a.password === password);
+        
+        if(userNameArray && checkPassword){
+            
+            res.send(userNameArray);
 
+        }
+        else if(!checkPassword){
 
+            res.send("Fel lösenord eller användarnamn.");
+            
+        }
+    
+    
     })
 
 
